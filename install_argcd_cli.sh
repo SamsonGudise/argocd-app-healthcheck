7c2c12c57e525b6e8c31634e2306546d655314fc
Samson Gudise <samson.gudise@stem.com>
2021-01-23 21:38:25 -0700

    v3

---
+++ install_argcd_cli.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p /usr/local/bin/
+curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
+chmod +x /usr/local/bin/argocd
