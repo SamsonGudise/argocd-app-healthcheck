351ddad1f4ee443f43d275c7e66dc341140fec73
Samson Gudise <samson.gudise@stem.com>
2021-01-23 21:58:45 -0700

    install instruction - script err

--- install_argocd_cli.sh
+++ install_argocd_cli.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+## Check  https://argoproj.github.io/argo-cd/cli_installation/
+## For latest version and install instructions
+
 mkdir -p /usr/local/bin/
-curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$VERSION/argocd-linux-amd64
-chmod +x /usr/local/bin/argocd
+VERSION=1.8.3
+curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64
+if [ $? -eq 0 ]; then
+    chmod +x /usr/local/bin/argocd
+fi
