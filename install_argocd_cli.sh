090ccf7207430bd5b46cc821ab4001b97b50a95a
Samson Gudise <samson.gudise@stem.com>
2021-01-23 22:20:43 -0700

    fix url

--- install_argocd_cli.sh
+++ install_argocd_cli.sh
@@ -3,7 +3,7 @@
 ## For latest version and install instructions
 
 mkdir -p /usr/local/bin/
-VERSION=1.8.3
+VERSION=v1.8.3
 curl -sSL -v -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64
 if [ $? -eq 0 ]; then
     chmod +x /usr/local/bin/argocd
