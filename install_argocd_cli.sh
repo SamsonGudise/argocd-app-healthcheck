14687598b32a5e64c19258231fd9e7d9484adaf7
Samson Gudise <samson.gudise@stem.com>
2021-01-24 18:24:49 -0700

    fix permission

--- install_argocd_cli.sh
+++ install_argocd_cli.sh
@@ -7,6 +7,6 @@ VERSION=v1.8.3
 #curl -sSL -v -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64
 wget https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64
 if [ $? -eq 0 ]; then
-    mv argocd-linux-amd64 /usr/local/bin/argocd
-    chmod +x /usr/local/bin/argocd
+    mv argocd-linux-amd64 /tmp/argocd
+    chmod +x /tmp/argocd
 fi
