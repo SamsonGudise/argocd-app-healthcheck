97db0ac4812f668eeebb6b52d4d3d472824e821d
Samson Gudise <samson.gudise@stem.com>
2021-01-24 17:43:55 -0700

    use wget instead

--- install_argocd_cli.sh
+++ install_argocd_cli.sh
@@ -4,7 +4,9 @@
 
 mkdir -p /usr/local/bin/
 VERSION=v1.8.3
-curl -sSL -v -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64
+#curl -sSL -v -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64
+wget https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64
 if [ $? -eq 0 ]; then
+    mv argocd-linux-amd64 /usr/local/bin/argocd
     chmod +x /usr/local/bin/argocd
 fi
