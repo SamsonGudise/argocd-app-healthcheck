1b5167578c2f37a46d8d463314da9ffae2a8d0ac
Samson Gudise <samson.gudise@stem.com>
2021-01-24 21:29:07 -0700

    include git revision validation

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,2 +1,23 @@
-#!/bin/sh -x
-/tmp/argocd app get $1
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+	echo "health_check.sh application_name revision"
+	exit 2
+fi
+x=0
+/tmp/argocd app diff $1 --revision $2 1>> /dev/null 2>>/dev/null
+while [[ $? -ne 0 && $x -le 10 ]];
+do
+	let x=x+1
+	sleep 30
+	/tmp/argocd app diff $1 --revision $2 1>> /dev/null 2>>/dev/null
+done
+
+if [ $? -eq 0 ]; then
+	hst=`/tmp/argocd app get $1  |awk '/^Health Status/' | awk '{print $3}'`
+	if [ $hst == 'Healthy' ]; then
+    		exit 0
+	fi
+	exit 1
+fi
+exit 1
--- install_argocd_cli.sh
+++ install_argocd_cli.sh
@@ -2,10 +2,8 @@
 ## Check  https://argoproj.github.io/argo-cd/cli_installation/
 ## For latest version and install instructions
 
-VERSION=v1.8.3
-curl -sSL -v -o /tmp/argocd https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64
-#wget https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64
+VERSION=$1
+curl -sSL -o /tmp/argocd https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64
 if [ $? -eq 0 ]; then
-    #mv argocd-linux-amd64 /tmp/argocd
     chmod +x /tmp/argocd
 fi
